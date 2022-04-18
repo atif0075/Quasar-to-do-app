a232d839b8ccee63ab0004bf8d42f0e0cff6eea1
M Atif <chatif476@gmail.com>
2022-04-18 16:52:03 +0500

    Build SPA -- ✨

--- .postcssrc.js
+++ .postcssrc.js
@@ -5,6 +5,5 @@ module.exports = {
   plugins: [
     // to edit target browsers: use "browserslist" field in package.json
     require('autoprefixer'),
-    require('tailwindcss')('./tailwind.config.js'),
   ]
 }
--- src/extensions/tailwindcss/tailwind.config.js
+++ src/extensions/tailwindcss/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [
+  purge: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
